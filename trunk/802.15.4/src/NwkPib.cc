7cc22f4a844ba266cb8bcf9ec248faee4ed8bae5
bernard.halas <bernard.halas@8c3b866e-11a2-11de-af30-3d3b2b6d83b0>
2009-04-02 14:40:54 +0000

    NwkPib constants and attributes declared

--- trunk/802.15.4/src/NwkPib.cc
+++ trunk/802.15.4/src/NwkPib.cc
@@ -1,14 +1,81 @@
 #include "NwkPib.h"
 
-Define_Module(NwkPib);
+Define_Module( NwkPib);
 
-void NwkPib::initialize()
-{
-	// TODO - Generated method body
+void NwkPib::initialize(int stage) {
+	BasicModule::initialize(stage);
+	if (stage == 0) {
+		nlmeOut = findGate("nlmeOut");
+		nlmeIn = findGate("nlmeIn");
+
+		resetNwkPibValues();
+		commentsLevel = COMMENT_ALL;
+	}
 }
 
-void NwkPib::handleMessage(cMessage *msg)
-{
-	// TODO - Generated method body
+void NwkPib::handleMessage(cMessage *msg) {
+	if (!msg->isSelfMessage()) {
+		commentMsgReceived(msg);
+	}
+	if (msg->getArrivalGateId() == nlmeIn) {
+		handleNlmeMsg(msg);
+	} else {
+		handleSelfMsg(msg);
+	}
 }
 
+void NwkPib::handleSelfMsg(cMessage *msg) {
+	delete (msg);
+}
+
+void NwkPib::handleNlmeMsg(cMessage *msg) {
+	std::string msgName = msg->getName();
+	delete (msg);
+}
+
+void NwkPib::sendNlme(cMessage *msg) {
+	commentMsgSending(msg);
+	sendDelayed(msg, 0.0, nlmeOut);
+}
+
+void NwkPib::resetNwkPibValues() {
+	setNwkcCoordinatorCapable(
+			((FFDAppLayer *) (this->getParentModule()->getParentModule()->getModuleByRelativePath(
+					"app")))->getRole() == COORDINATOR);
+	setNwkcDiscoveryRetryLimit(0x03);
+	setNwkcMaxDepth(0x0F);
+	setNwkcMinHeaderOverhead(0x08);
+	setNwkcProtocolVersion(0x01);
+	setNwkcRepairThreshold(0x03);
+	setNwkcRouteDiscoveryTime(0x2710);
+	setNwkcMaxBroadcastJitter(0x40);
+	setNwkcInitialRreqRetries(0x03);
+	setNwkcRreqRetries(0x02);
+	setNwkcRreqRetryInterval(0xFE);
+	setNwkcMinRreqJitter(0x01);
+	setNwkcMaxRreqJitter(0x40);
+	setNwkSequenceNumber((unsigned char)(rand()%256));
+	setNwkPassiveAckTimeout(0x03);
+	setNwkMaxBroadcastRetries(0x03);
+	setNwkMaxChildren(0x07);
+	setNwkMaxDepth(0x05);
+	setNwkMaxRouters(0x05);
+	setNwkNetworkBroadcastDeliveryTime(getNwkPassiveAckTimeout()*getNwkMaxBroadcastRetries());
+	setNwkReportConstantCost(0x00);
+	setNwkRouteDiscoveryRetriesPermitted(getNwkcDiscoveryRetryLimit());
+	setNwkSymLink(false);
+	NwkCapabilityInformation nci;
+	nci.alternatePanCoordinator = 0x00;
+	nci.deviceType = 0x00;
+	nci.powerSource = 0x00;
+	nci.receiverOnWhenIdle = 0x00;
+	nci.securityCapability = 0x00;
+	nci.allocateAddress = 0x00;
+	setNwkCapabilityInformation(nci);
+	setNwkUseTreeAddrAlloc(true);
+	setNwkUseTreeRouting(true);
+	setNwkNextAddress(0x0000);
+	setNwkAvailableAddress(0x0000);
+	setNwkAddressIncrement(0x0001);
+	setNwkTransactionPersistenceTime(0x01f4);
+}
--- trunk/802.15.4/src/NwkPib.h
+++ trunk/802.15.4/src/NwkPib.h
@@ -2,15 +2,389 @@
 #define __NWKPIB_H__
 
 #include <omnetpp.h>
+#include "BasicModule.h"
+#include "typedef.h"
+#include "Messages_m.h"
+#include "BBItems.h"
+#include <string>
+#include <sstream>
+#include "FFDAppLayer.h"
 
-/**
- * TODO - Generated class
- */
-class NwkPib : public cSimpleModule
-{
-  protected:
-    virtual void initialize();
-    virtual void handleMessage(cMessage *msg);
+class NwkPib: public BasicModule {
+public:
+	virtual void initialize(int);
+	virtual void handleMessage(cMessage *msg);
+protected:
+	/** @brief gate id*/
+	/*@{*/
+	int nlmeOut;
+	int nlmeIn;
+	/*@}*/
+	/** NWK PIB constants */
+	bool nwkcCoordinatorCapable;
+	/** @fixme ommiting the nwkcDefaultSecurityLevel */
+	unsigned char nwkcDiscoveryRetryLimit;
+	unsigned char nwkcMaxDepth;
+	unsigned char nwkcMinHeaderOverhead;
+	unsigned char nwkcProtocolVersion;
+	unsigned char nwkcRepairThreshold;
+	unsigned short nwkcRouteDiscoveryTime;
+	unsigned char nwkcMaxBroadcastJitter;
+	unsigned char nwkcInitialRreqRetries;
+	unsigned char nwkcRreqRetries;
+	unsigned char nwkcRreqRetryInterval;
+	unsigned char nwkcMinRreqJitter;
+	unsigned char nwkcMaxRreqJitter;
+	/** NWK PIB attributes */
+	unsigned char nwkSequenceNumber;
+	unsigned char nwkPassiveAckTimeout;
+	unsigned char nwkMaxBroadcastRetries;
+	unsigned char nwkMaxChildren;
+	unsigned char nwkMaxDepth;
+	unsigned char nwkMaxRouters;
+	/** @todo network table belongs here... */
+	unsigned char nwkNetworkBroadcastDeliveryTime;
+	unsigned char nwkReportConstantCost;
+	unsigned char nwkRouteDiscoveryRetriesPermitted;
+	/** @todo route table belongs here... */
+	bool nwkSymLink;
+	NwkCapabilityInformation nwkCapabilityInformation;
+	bool nwkUseTreeAddrAlloc;
+	bool nwkUseTreeRouting;
+	unsigned short nwkNextAddress;
+	unsigned short nwkAvailableAddress;
+	unsigned short nwkAddressIncrement;
+	unsigned short nwkTransactionPersistenceTime;
+	/** @brief Sets the level of comments to the EV output */
+	CommentsLevel commentsLevel;
+	virtual void handleSelfMsg(cMessage*);
+	virtual void handleNlmeMsg(cMessage*);
+	virtual void sendNlme(cMessage*);
+	virtual void resetNwkPibValues();
+	virtual void comment(CommentsLevel level, std::string s) {
+		if ((level & commentsLevel) > COMMENT_NOTHING) {
+			/** @todo align logName substrings for routers and endDevices */
+			std::cout << logName() << "." << getName() << "\t" << s << endl;
+		}
+	}
+	virtual void commentMsgSending(cMessage *msg) {
+		std::stringstream commentStream;
+		commentStream << "Sending " << msg->getName();
+		comment(COMMENT_MESSAGE, commentStream.str());
+	}
+	virtual void commentMsgReceived(cMessage *msg) {
+		std::stringstream commentStream;
+		commentStream << "Received " << msg->getName();
+		comment(COMMENT_MESSAGE, commentStream.str());
+	}
+	virtual void commentError(char* errorMessage) {
+		std::stringstream commentStream;
+		commentStream << "ERROR: " << errorMessage;
+		comment(COMMENT_ERROR, commentStream.str());
+	}
+public:
+	NwkEnum setPibAttribute(PibIdentifier, unsigned int*);
+	unsigned int* getPibAttribute(PibIdentifier);
+    bool getNwkcCoordinatorCapable() const
+    {
+        return nwkcCoordinatorCapable;
+    }
+
+    void setNwkcCoordinatorCapable(bool nwkcCoordinatorCapable)
+    {
+        this->nwkcCoordinatorCapable = nwkcCoordinatorCapable;
+    }
+
+    unsigned char getNwkcDiscoveryRetryLimit() const
+    {
+        return nwkcDiscoveryRetryLimit;
+    }
+
+    void setNwkcDiscoveryRetryLimit(unsigned char nwkcDiscoveryRetryLimit)
+    {
+        this->nwkcDiscoveryRetryLimit = nwkcDiscoveryRetryLimit;
+    }
+
+    unsigned char getNwkcMaxDepth() const
+    {
+        return nwkcMaxDepth;
+    }
+
+    void setNwkcMaxDepth(unsigned char nwkcMaxDepth)
+    {
+        this->nwkcMaxDepth = nwkcMaxDepth;
+    }
+
+    unsigned char getNwkcMinHeaderOverhead() const
+    {
+        return nwkcMinHeaderOverhead;
+    }
+
+    void setNwkcMinHeaderOverhead(unsigned char nwkcMinHeaderOverhead)
+    {
+        this->nwkcMinHeaderOverhead = nwkcMinHeaderOverhead;
+    }
+
+    unsigned char getNwkcProtocolVersion() const
+    {
+        return nwkcProtocolVersion;
+    }
+
+    void setNwkcProtocolVersion(unsigned char nwkcProtocolVersion)
+    {
+        this->nwkcProtocolVersion = nwkcProtocolVersion;
+    }
+
+    unsigned char getNwkcRepairThreshold() const
+    {
+        return nwkcRepairThreshold;
+    }
+
+    void setNwkcRepairThreshold(unsigned char nwkcRepairThreshold)
+    {
+        this->nwkcRepairThreshold = nwkcRepairThreshold;
+    }
+
+    unsigned short getNwkcRouteDiscoveryTime() const
+    {
+        return nwkcRouteDiscoveryTime;
+    }
+
+    void setNwkcRouteDiscoveryTime(unsigned short  nwkcRouteDiscoveryTime)
+    {
+        this->nwkcRouteDiscoveryTime = nwkcRouteDiscoveryTime;
+    }
+
+    unsigned char getNwkcMaxBroadcastJitter() const
+    {
+        return nwkcMaxBroadcastJitter;
+    }
+
+    void setNwkcMaxBroadcastJitter(unsigned char nwkcMaxBroadcastJitter)
+    {
+        this->nwkcMaxBroadcastJitter = nwkcMaxBroadcastJitter;
+    }
+
+    unsigned char getNwkcInitialRreqRetries() const
+    {
+        return nwkcInitialRreqRetries;
+    }
+
+    void setNwkcInitialRreqRetries(unsigned char nwkcInitialRreqRetries)
+    {
+        this->nwkcInitialRreqRetries = nwkcInitialRreqRetries;
+    }
+
+    unsigned char getNwkcRreqRetries() const
+    {
+        return nwkcRreqRetries;
+    }
+
+    void setNwkcRreqRetries(unsigned char nwkcRreqRetries)
+    {
+        this->nwkcRreqRetries = nwkcRreqRetries;
+    }
+
+    unsigned char getNwkcRreqRetryInterval() const
+    {
+        return nwkcRreqRetryInterval;
+    }
+
+    void setNwkcRreqRetryInterval(unsigned char nwkcRreqRetryInterval)
+    {
+        this->nwkcRreqRetryInterval = nwkcRreqRetryInterval;
+    }
+
+    unsigned char getNwkcMinRreqJitter() const
+    {
+        return nwkcMinRreqJitter;
+    }
+
+    void setNwkcMinRreqJitter(unsigned char nwkcMinRreqJitter)
+    {
+        this->nwkcMinRreqJitter = nwkcMinRreqJitter;
+    }
+
+    unsigned char getNwkcMaxRreqJitter() const
+    {
+        return nwkcMaxRreqJitter;
+    }
+
+    void setNwkcMaxRreqJitter(unsigned char nwkcMaxRreqJitter)
+    {
+        this->nwkcMaxRreqJitter = nwkcMaxRreqJitter;
+    }
+
+    unsigned char getNwkSequenceNumber() const
+    {
+        return nwkSequenceNumber;
+    }
+
+    void setNwkSequenceNumber(unsigned char nwkSequenceNumber)
+    {
+        this->nwkSequenceNumber = nwkSequenceNumber;
+    }
+
+    unsigned char getNwkPassiveAckTimeout() const
+    {
+        return nwkPassiveAckTimeout;
+    }
+
+    void setNwkPassiveAckTimeout(unsigned char nwkPassiveAckTimeout)
+    {
+        this->nwkPassiveAckTimeout = nwkPassiveAckTimeout;
+    }
+
+    unsigned char getNwkMaxBroadcastRetries() const
+    {
+        return nwkMaxBroadcastRetries;
+    }
+
+    void setNwkMaxBroadcastRetries(unsigned char nwkMaxBroadcastRetries)
+    {
+        this->nwkMaxBroadcastRetries = nwkMaxBroadcastRetries;
+    }
+
+    unsigned char getNwkMaxChildren() const
+    {
+        return nwkMaxChildren;
+    }
+
+    void setNwkMaxChildren(unsigned char nwkMaxChildren)
+    {
+        this->nwkMaxChildren = nwkMaxChildren;
+    }
+
+    unsigned char getNwkMaxDepth() const
+    {
+        return nwkMaxDepth;
+    }
+
+    void setNwkMaxDepth(unsigned char nwkMaxDepth)
+    {
+        this->nwkMaxDepth = nwkMaxDepth;
+    }
+
+    unsigned char getNwkMaxRouters() const
+    {
+        return nwkMaxRouters;
+    }
+
+    void setNwkMaxRouters(unsigned char nwkMaxRouters)
+    {
+        this->nwkMaxRouters = nwkMaxRouters;
+    }
+
+    unsigned char getNwkNetworkBroadcastDeliveryTime() const
+    {
+        return nwkNetworkBroadcastDeliveryTime;
+    }
+
+    void setNwkNetworkBroadcastDeliveryTime(unsigned char nwkNetworkBroadcastDeliveryTime)
+    {
+        this->nwkNetworkBroadcastDeliveryTime = nwkNetworkBroadcastDeliveryTime;
+    }
+
+    unsigned char getNwkReportConstantCost() const
+    {
+        return nwkReportConstantCost;
+    }
+
+    void setNwkReportConstantCost(unsigned char nwkReportConstantCost)
+    {
+        this->nwkReportConstantCost = nwkReportConstantCost;
+    }
+
+    unsigned char getNwkRouteDiscoveryRetriesPermitted() const
+    {
+        return nwkRouteDiscoveryRetriesPermitted;
+    }
+
+    void setNwkRouteDiscoveryRetriesPermitted(unsigned char nwkRouteDiscoveryRetriesPermitted)
+    {
+        this->nwkRouteDiscoveryRetriesPermitted = nwkRouteDiscoveryRetriesPermitted;
+    }
+
+    bool getNwkSymLink() const
+    {
+        return nwkSymLink;
+    }
+
+    void setNwkSymLink(bool nwkSymLink)
+    {
+        this->nwkSymLink = nwkSymLink;
+    }
+
+    NwkCapabilityInformation getNwkCapabilityInformation() const
+    {
+        return nwkCapabilityInformation;
+    }
+
+    void setNwkCapabilityInformation(NwkCapabilityInformation nwkCapabilityInformation)
+    {
+        this->nwkCapabilityInformation = nwkCapabilityInformation;
+    }
+
+    bool getNwkUseTreeAddrAlloc() const
+    {
+        return nwkUseTreeAddrAlloc;
+    }
+
+    void setNwkUseTreeAddrAlloc(bool nwkUseTreeAddrAlloc)
+    {
+        this->nwkUseTreeAddrAlloc = nwkUseTreeAddrAlloc;
+    }
+
+    bool getNwkUseTreeRouting() const
+    {
+        return nwkUseTreeRouting;
+    }
+
+    void setNwkUseTreeRouting(bool nwkUseTreeRouting)
+    {
+        this->nwkUseTreeRouting = nwkUseTreeRouting;
+    }
+
+    unsigned short getNwkNextAddress() const
+    {
+        return nwkNextAddress;
+    }
+
+    void setNwkNextAddress(unsigned short  nwkNextAddress)
+    {
+        this->nwkNextAddress = nwkNextAddress;
+    }
+
+    unsigned short getNwkAvailableAddress() const
+    {
+        return nwkAvailableAddress;
+    }
+
+    void setNwkAvailableAddress(unsigned short  nwkAvailableAddress)
+    {
+        this->nwkAvailableAddress = nwkAvailableAddress;
+    }
+
+    unsigned short getNwkAddressIncrement() const
+    {
+        return nwkAddressIncrement;
+    }
+
+    void setNwkAddressIncrement(unsigned short  nwkAddressIncrement)
+    {
+        this->nwkAddressIncrement = nwkAddressIncrement;
+    }
+
+    unsigned short getNwkTransactionPersistenceTime() const
+    {
+        return nwkTransactionPersistenceTime;
+    }
+
+    void setNwkTransactionPersistenceTime(unsigned short  nwkTransactionPersistenceTime)
+    {
+        this->nwkTransactionPersistenceTime = nwkTransactionPersistenceTime;
+    }
+
 };
 
 #endif
